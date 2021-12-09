f3b59d14f431473fe9f1eb04cc79b9a931fc53f8
vrushabhmungelwar <vrushabhmungelwar10@gmail.com>
2021-12-09 22:11:59 +0530

    fixed error

--- index.js
+++ index.js
@@ -12,32 +12,9 @@ const PORT = process.env.PORT;
 // app.use(cors());
 
 
-
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://affectionate-murdock-39034f.netlify.app');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-
-
-// app.use(
-//   cors({ origin: 'https://affectionate-murdock-39034f.netlify.app' })
-// );
+app.use(
+  cors({ origin: 'https://affectionate-murdock-39034f.netlify.app' })
+);
 app.use(express.json());
 
 const MONGO_URL = process.env.MONGO_URL;
