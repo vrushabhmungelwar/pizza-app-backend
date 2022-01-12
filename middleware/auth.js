97c475c9ed072d47b2646b1cf4099fbcda326f23
Vrushabh Mungelwar <vrushabhmungelwar10@gmail.com>
2022-01-12 23:11:31 +0530

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const auth = (request, response, next) => {
   try {
-    const token = request.header("x-auth-token");
+    const token = request.header("authtoken");
     console.log("token", token);
     jwt.verify(token, process.env.SECRET_KEY);
     next();
